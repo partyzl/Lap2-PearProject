66b64f9b19b559370c5f419190e2aef0a44f2cb6
Parth Patel <parth6796@yahoo.com>
2021-08-24 14:50:55 +0100

    done the schema and seed files

---
+++ db/2_seed.sql
@@ -0,0 +1,6 @@
+INSERT INTO posts (title, pseudonym, body)
+VALUES(
+    ('cars', 'Johnny Cash', 'I found a car I really liked today, I''m going to buy it'),
+    ('baking', 'Rebel Hans', 'I have a new found love for making cookies, my greatest creation are scone troopers'),
+    ('football', 'Dele Alli', 'I suck at football')
+);
